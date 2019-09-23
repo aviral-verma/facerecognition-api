0f978289a43ad37296fcb73c6bf0c9dbaf944a76
Aviral Verma <aviralverma.verma@gmail.com>
2019-09-23 12:18:32 +0530

    updating database

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ const image = require('./controllers/image');
 const db = knex({
     client: 'pg',
     connection: {
-      host : '127.0.0.1',
+      host : 'postgresql-convex-78645',
       user : 'aviraliitk',
       password : 'your_database_password',
       database : 'face-recognition'
