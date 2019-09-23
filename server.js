93f55163ab1602daa4cef281afc8da527cb75706
Aviral Verma <aviralverma.verma@gmail.com>
2019-09-23 12:20:26 +0530

    adding database connection

--- server.js
+++ server.js
@@ -12,10 +12,8 @@ const image = require('./controllers/image');
 const db = knex({
     client: 'pg',
     connection: {
-      host : 'postgresql-convex-78645',
-      user : 'aviraliitk',
-      password : 'your_database_password',
-      database : 'face-recognition'
+      host : 'process.env.DATABASE_URL',
+      ssl: true,
     }
   });
 
